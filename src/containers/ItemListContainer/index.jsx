75ce3b14998dc92d886c566b0d1a8915b37d2676
JoseSalado <saladojosemiguel@gmail.com>
2022-11-09 20:21:45 -0300

    agregue los items

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import './styles.css';
 
 const ItemListContainer = ({greeting})=>{
+
+    const [nombre , setNombre] = useState("Sorrentinos")
+
+    const cambiarNombre =()=>{
+        if (nombre === "Sorrentinos") setNombre("Ravioles")
+        else setNombre ("Sorrentinos")
+    }
     return(
         <div className='item-list-container'>
             <h2>{greeting}</h2>
+            <p>{nombre}</p>
+            <button onClick={cambiarNombre}>Cambiar de Producto</button>
         </div>
     )
 
